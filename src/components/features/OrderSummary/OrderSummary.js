3109d848c0c9dd6684a61a7a9c5fd3c01e4f3c8e
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-07-01 15:05:16 +0200

    Add price - OrderFormContainer

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './OrderSummary.scss';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
-class OrderSummary extends React.Component {
-  render(){
-    return(
-      <div>
-        <h2 className={styles.component}>Total:<strong>$11,111</strong></h2>
-      </div>
-    );
-  }
-}
+const OrderSummary = props => {
+  return(
+    <h2 className={styles.component}>Total:<strong>{calculateTotal(formatPrice(props.tripCost), props.options)}</strong></h2>
+  );
+};
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderSummary;
