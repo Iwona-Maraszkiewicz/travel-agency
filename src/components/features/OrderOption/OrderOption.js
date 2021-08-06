838429c09b259f4e5ed525aea7ec0b7fdce06142
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-08-06 11:43:21 +0200

    Add tests - OrderOption

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
-import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 import OrderOptionText from './OrderOptionText';
 import OrderOptionDate from './OrderOptionDate';
 
@@ -13,28 +12,24 @@ const optionTypes = {
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
-  text : OrderOptionText,
+  text: OrderOptionText,
   date: OrderOptionDate,
 };
-
-const OrderOption = ({name, type, id, setOrderOption, ...otherProps}) => {
+const OrderOption = ({ name, type, id, setOrderOption, ...otherProps }) => {
   const OptionComponent = optionTypes[type];
-  if(!OptionComponent){
+  if (!OptionComponent) {
     return null;
   } else {
     return (
       <div className={styles.component}>
         <h3 className={styles.title}>{name}</h3>
         <OptionComponent
-          {...otherProps} setOptionValue={value => setOrderOption({[id]: value})}
+          setOptionValue={(value) => setOrderOption({ [id]: value })}
+          {...otherProps}
         />
       </div>
     );
   }
-};
-
-OrderOption.propTypes = {
-  name: PropTypes.string,
 };
 
 export default OrderOption;
--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import DatePicker from 'react-datepicker';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 class OrderOptionDate extends React.Component {
   state = {
     startDate: new Date(),
   };
+  static propTypes = {
+    setOptionValue: PropTypes.func,
+  };
 
-  handleChange = date => {
+  handleChange = (date) => {
     this.setState({
       startDate: date,
     });
@@ -24,9 +27,4 @@ class OrderOptionDate extends React.Component {
     );
   }
 }
-
-OrderOptionDate.propTypes = {
-  setOptionValue: PropTypes.func,
-};
-
 export default OrderOptionDate;
